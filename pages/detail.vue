fc70145d4f48befe2e98b909a1a74614b949aa1e
tianjia <1044880118@qq.com>
2022-02-08 10:14:30 +0800

    first commit

---
+++ pages/detail.vue
@@ -0,0 +1,21 @@
+<template>
+  <a-row type="flex" justify="center" align="top">
+    <a-col :span="6">
+      <p>params路由传参：{{ $route.params.keyword }}</p>
+    </a-col>
+    <a-col :span="6">
+      <p>query跳转链接传参：{{ $route.query.id }}</p>
+    </a-col>
+  </a-row>
+</template>
+
+<script>
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "detail",
+});
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <a-row type="flex" justify="center" align="top">
+    <a-col :span="12">
+      <div class="">
+        <h2>路由跳转</h2>
+        <nuxt-link to="/detail">常规不传参</nuxt-link>
+        <br>
+        <br>
+        <div class="search-box">
+          <a-input placeholder="搜索关键词" v-model="searchKeyword" />
+          <nuxt-link
+            :to="{ name: 'detail', params: { keyword: searchKeyword } }"
+            >params路由传参</nuxt-link
+          >
+        </div>
+        <br>
+        <nuxt-link :to="{ path: '/detail', query: { id: 1 } }"
+          >query跳转链接传参</nuxt-link
+        >
+      </div>
+    </a-col>
+  </a-row>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "IndexPage",
+  data() {
+    return {
+      searchKeyword: "",
+    };
+  },
+});
+</script>
