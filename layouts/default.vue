356bdd28f70c74529b6cea5606bcd36cf6b60adb
tianjia <1044880118@qq.com>
2022-02-11 09:59:57 +0800

    做了一点点样式的调整

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,39 @@
 <template>
-    <div class="">
-        <h1>1111</h1>
+  <div id="components-layout-demo-basic">
+    <a-layout>
+      <Header />
+      <a-layout-content>
         <nuxt />
-        <h1>332</h1>
-    </div>
+      </a-layout-content>
+      <Footer />
+    </a-layout>
+  </div>
 </template>
 
 <script>
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
 export default {
-
-}
+  components: [Header, Footer],
+};
 </script>
 
-<style>
-
+<style  lang="less" scoped>
+#components-layout-demo-basic {
+  text-align: center;
+  display: flex;
+  height: 100%;
+  .ant-layout-header {
+    background: #7dbcea;
+    color: #fff;
+  }
+  .ant-layout-footer {
+    background: #7dbcea;
+    color: #fff;
+  }
+  .ant-layout-content {
+    flex: 1;
+    padding: 40px;
+  }
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,40 @@
 <template>
-  <a-row type="flex" justify="center" align="top">
-    <a-col :span="12">
-      <div class="content">
-        <h2>路由跳转</h2>
-        <nuxt-link to="/detail">常规不传参</nuxt-link>
-        <br />
-        <br />
-        <div class="search-box">
-          <a-input placeholder="搜索关键词" v-model="searchKeyword" />
-          <nuxt-link
-            class="right-link"
-            :to="{ name: 'detail', params: { keyword: searchKeyword } }"
-            >params路由传参</nuxt-link
-          >
-        </div>
-        <br />
-        <nuxt-link :to="{ path: '/detail', query: { id: 1 } }"
-          >query跳转链接传参</nuxt-link
-        >
+  <div class="">
+    <div class="btns">
+      <div class="btn">
+        <a-button type="primary" block>
+          <nuxt-link to="/detail">常规不传参</nuxt-link>
+        </a-button>
       </div>
-    </a-col>
-  </a-row>
+      <div class="btn">
+        <a-input-search
+          placeholder="input search text"
+          size="large"
+          v-model="searchKeyword"
+        >
+          <a-button slot="enterButton" type="primary" block>
+            <nuxt-link
+              class="right-link"
+              :to="{ name: 'detail', params: { keyword: searchKeyword } }"
+              >params路由传参</nuxt-link
+            >
+          </a-button>
+        </a-input-search>
+      </div>
+      <div class="btn">
+        <a-button type="danger" block>
+          <nuxt-link :to="{ path: '/detail', query: { id: 1 } }"
+            >query跳转链接传参</nuxt-link
+          >
+        </a-button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-
+import Carousel from "@/components/Carousel.vue";
 export default Vue.extend({
   name: "IndexPage",
   data() {
@@ -37,18 +46,15 @@ export default Vue.extend({
 </script>
 
 <style lang="less" scoped>
-.content {
-  // background-color: #cccccc;
-  h2 {
-    font-size: 20px;
-    color: @orange;
-  }
-  .search-box{
-    display: flex;
-    align-items: center;
-    .right-link{
-      width: 200px;
-      text-align: right;
+.btns {
+  display: flex;
+  width: 100%;
+  align-items: center;
+  .btn {
+    flex: 1;
+    padding: 0 10px;
+    /deep/input {
+      color: @green;
     }
   }
 }
